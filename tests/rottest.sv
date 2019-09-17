499d63711e03c8d36eb25823baff944c06d08006
Chris Kiefer <chrisk13fer@gmail.com>
2019-09-17 16:45:02 +0200

    env seq

--- tests/rottest.sv
+++ tests/rottest.sv
@@ -1,4 +1,7 @@
 //iverilog -o rot.vvp rottest.sv ../bitseq.sv; vvp rot.vvp -lxt
+//iverilog -g2012 -o env.vvp rottest.sv ../bitseq.sv ../envseq.sv; vvp env.vvp -lxt
+
+`include "../globals.vh"
 
 `timescale 10ns/1ns
 
@@ -15,10 +18,21 @@ module test;
     .clk(clk),       //clock signal
     .ena(1'b1),       //enable signal
     .rst(1'b0),       //reset signal
-    .data_in(9'b100011001),   //input
-    .len(24'd3),
+    .data_in(9'b101001000),   //input
+    .len(24'd5),
     .data_out(seqOut)  //output
   );
+
+  reg signed [`BITS-1:0] envVal;
+  envseq #(.DEPTH(4), .LEN(4), .TSCALE(2)) env1 (
+    .clk(clk),
+    .trigger(seqOut),
+    .ena(1'b1),       //enable signal
+    .rst(1'b0),       //reset signal
+    .levels(16'hFAF1),
+    .times(12'h324),
+    .envOut(envVal)
+    );
 
 
   initial begin
